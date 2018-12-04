c5aff74d135a5b34fa69bb957cdfb1c31d49b650
Alex Kharouk <akharoukpp@gmail.com>
2018-12-04 15:38:47 +0000

    REFACTOR fetch so it works inside index.js and makes the call

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser");
-const session = require("express-session");
 
 const app = express();
 
@@ -9,21 +7,39 @@ const app = express();
 const port = 3000;
 
 // Middleware
+const session = require("express-session");
+const fetch = require("node-fetch");
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: "banana bread" }));
 
 // Scripts
-const fetchUser = require("./public/javascripts/githubAPI");
+const fetchUser = require("./javascripts/API/fetchUser");
 
 app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Github Language Picker" });
+  const result = req.session.result;
+  console.log(result);
+  if (result !== undefined) {
+    res.render("index", { title: "Github Language Picker" });
+  } else {
+    res.render("index", {
+      title: "Github Language Picker"
+    });
+  }
 });
 
 app.post("/user", (req, res) => {
-  const result = fetchUser(req.body.gitUser);
+  req.session.result = fetch(
+    `https://api.github.com/search/users?q=${req.body.gitUser}`
+  )
+    .then(response => response.json())
+    .then(myJson => {
+      return JSON.stringify(myJson);
+    });
   res.redirect("/");
 });
 
--- javascripts/API/fetchUser.js
+++ javascripts/API/fetchUser.js
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-const fetchUser = (user) => {
+const fetchUser = user => {
   fetch(`https://api.github.com/search/users?q=${user}`)
     .then(response => response.json())
     .then(myJson => JSON.stringify(myJson));
