3e5ec5f2df7670b444f92fd018ca109bae074f21
Alex Kharouk <akharoukpp@gmail.com>
2018-12-04 12:00:13 +0000

    Get Express App Up and Running

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
+const port = 3000;
 const app = express();
-const indexRoute = require("./routes/index");
 
-app.use(express.static(path.join(__dirname, "/public")));
-app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index', { title: 'Github Language Picker' }));
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
