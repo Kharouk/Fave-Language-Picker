cc7ce5d9f5916b3afd4e21cb4fd776ef15766dfb
Alex Kharouk <akharoukpp@gmail.com>
2018-12-05 19:37:44 +0000

    FEATURE Create method that can find user's most common language.

--- index.js
+++ index.js
@@ -33,7 +33,38 @@ app.post('/user', (req, res) => {
 
 app.get('/results/', (req, res) => {
   const data = req.session.result;
-  res.render('result', { username: data[0].owner.login, repos: data });
+  const aryOfLanguages = [];
+  data.forEach((repo) => {
+    aryOfLanguages.push(repo.language);
+  });
+
+  res.render('result', {
+    username: data[0].owner.login,
+    repos: data,
+    language: findCommonLanguage(aryOfLanguages),
+  });
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
+
+const findCommonLanguage = (aryOfLanguages) => {
+  let maxCount = 1;
+  let langCount = 0;
+  let result;
+  for (let i = 0; i < aryOfLanguages.length; i += 1) {
+    for (let j = i; j < aryOfLanguages.length; j += 1) {
+      if (aryOfLanguages[i] === aryOfLanguages[j]) {
+        langCount += 1;
+        if (maxCount < langCount) {
+          maxCount = langCount;
+          result = aryOfLanguages[i];
+        }
+      }
+    }
+    langCount = 0;
+  }
+  if (result != null) {
+    return result;
+  }
+  return 'Sorry, you prefer to write READMEs!';
+};
