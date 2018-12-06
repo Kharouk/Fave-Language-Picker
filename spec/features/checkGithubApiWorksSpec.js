42bc6b7a24e188618cb3c670716cecd237539053
Alex Kharouk <akharoukpp@gmail.com>
2018-12-06 09:42:22 +0000

    REFACTOR Feature Tests and TEST edgecase if user's common lang is null

--- spec/features/checkGithubApiWorksSpec.js
+++ spec/features/checkGithubApiWorksSpec.js
@@ -1,3 +1,9 @@
+const searchUser = (username) => {
+  browser.get('/');
+  browser.driver.findElement(by.css('#git-user')).sendKeys(username);
+  browser.driver.findElement(by.css('.submit-user')).click();
+};
+
 describe('Protractor Demo App', () => {
   beforeEach(() => {
     browser.ignoreSynchronization = true;
@@ -10,25 +16,25 @@ describe('Protractor Demo App', () => {
   });
 
   it('Search for User', () => {
-    browser.get('/');
-    browser.driver.findElement(by.css('#git-user')).sendKeys('kharouk');
-    browser.driver.findElement(by.css('.submit-user')).click();
+    searchUser('kharouk');
     const title = browser.driver.findElement(by.css('#username')).getText();
     expect(title).toContain('Kharouk');
   });
 
   it('Search for User and get language', () => {
-    browser.get('/');
-    browser.driver.findElement(by.css('#git-user')).sendKeys('alex');
-    browser.driver.findElement(by.css('.submit-user')).click();
+    searchUser('alex');
     const language = browser.driver.findElement(by.css('#language')).getText();
     expect(language).toContain('Python');
   });
 
+  it("Returns 'READMEs' if user's common language is null", () => {
+    searchUser('wesbos');
+    const language = browser.driver.findElement(by.css('#language')).getText();
+    expect(language).toContain('READMEs!');
+  });
+
   it('Returns Error Message if No Username', () => {
-    browser.get('/');
-    browser.driver.findElement(by.css('#git-user')).sendKeys('jefwifweji9feh');
-    browser.driver.findElement(by.css('.submit-user')).click();
+    searchUser('dsfhjewiofhiweh');
     const error = browser.driver.findElement(by.css('.alert')).getText();
     expect(error).toContain("Sorry, that GitHub username doesn't exist. Try again!");
   });
