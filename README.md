343550e0eeffbd8ebe974cf018046ab3154e0123
Alex Kharouk <akharoukpp@gmail.com>
2018-12-06 09:39:04 +0000

    TEST e2e features when searching for users and getting most-used language.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Favourite Language Picker
+[![Build Status](https://travis-ci.com/Kharouk/Fave-Language-Picker.svg?token=y9Amnq7P4ASRBhz6qGVw&branch=master)](https://travis-ci.com/Kharouk/Fave-Language-Picker)
+
 Unsure on what your favourite programming language is? Have millions of repos and still guessing if you use Ruby over JavaScript? Use this renowned application to figure out your most used language! 
 
 
--- spec/features/checkGithubApiWorksSpec.js
+++ spec/features/checkGithubApiWorksSpec.js
@@ -3,9 +3,33 @@ describe('Protractor Demo App', () => {
     browser.ignoreSynchronization = true;
   });
 
-  it('should have a title', () => {
+  it('Title: Github Language Picker', () => {
     browser.get('/');
     const title = browser.driver.findElement(by.css('#title')).getText();
     expect(title).toContain('Github Language Picker');
+  });
+
+  it('Search for User', () => {
+    browser.get('/');
+    browser.driver.findElement(by.css('#git-user')).sendKeys('kharouk');
+    browser.driver.findElement(by.css('.submit-user')).click();
+    const title = browser.driver.findElement(by.css('#username')).getText();
+    expect(title).toContain('Kharouk');
+  });
+
+  it('Search for User and get language', () => {
+    browser.get('/');
+    browser.driver.findElement(by.css('#git-user')).sendKeys('alex');
+    browser.driver.findElement(by.css('.submit-user')).click();
+    const language = browser.driver.findElement(by.css('#language')).getText();
+    expect(language).toContain('Python');
+  });
+
+  it('Returns Error Message if No Username', () => {
+    browser.get('/');
+    browser.driver.findElement(by.css('#git-user')).sendKeys('jefwifweji9feh');
+    browser.driver.findElement(by.css('.submit-user')).click();
+    const error = browser.driver.findElement(by.css('.alert')).getText();
+    expect(error).toContain("Sorry, that GitHub username doesn't exist. Try again!");
   });
 });
