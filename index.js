ee50c6a85cb6a3d1dd90f39420117518b9301302
Alex Kharouk <akharoukpp@gmail.com>
2018-12-06 10:00:43 +0000

    FIX port for Heroku.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Default Dev Port
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 const session = require('express-session');
